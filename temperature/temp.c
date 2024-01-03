37c67ccbcc02cb41eeebead9ecf408b9f817ea85
Joon <jyeo41@gmail.com>
2024-01-03 01:21:49 +0000

    added new program Temperature

---
+++ temperature/temp.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/* print Fahrenheit to Celsius table 
+   in steps of 20 starting from fahr = 0 */
+
+int main()
+{
+	int fahr, celsius;
+	
+	for(fahr = 0; fahr <= 300; fahr+= 20)
+	{
+		celsius = 5 * (fahr-32) / 9;
+		printf("%d\t%d\n", fahr, celsius);
+	}
+}
